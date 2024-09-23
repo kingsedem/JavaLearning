2fc766676a6ca4c41cbd8771bdc6ac002db0d04f
kingsley <kingsleyedemudoh@yahoo.com>
2024-09-23 12:00:15 +0100

    added sum method

--- src/main/java/PodTest.java
+++ src/main/java/PodTest.java
@@ -3,9 +3,16 @@ import java.sql.SQLOutput;
 public class PodTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello SDET's");
+        System.out.println("My name is Kingsley");
+        //sum();
+        //PodTest.sum();
 
-
+        PodTest p1 = new PodTest();
+        p1.sum();
+    }
+    public void sum(){
+           int c = 5 + 6;
+         System.out.println("Sum is: "+c);
     }
 }
 
