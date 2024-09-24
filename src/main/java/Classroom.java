8e037c07ba0c51f42939e9f246d21bf25f30525f
kingsley <kingsleyedemudoh@yahoo.com>
2024-09-24 11:40:21 +0100

    added data types

--- src/main/java/Classroom.java
+++
@@ -1,8 +0,0 @@
-public class Classroom {
-    int  floorNumber;
-    int seatCount;
-
-    public void clean(){
-        System.out.println("Students clean their classrooms");
-    }
-}
--- src/main/java/Teacher.java
+++
@@ -1,8 +0,0 @@
-public class Teacher {
-    String name;
-    String subject;
-
-    public void teaching(){
-        System.out.println("Teach can teach");
-    }
-}
