bd2283c69bca23c3e6381026891f5112af97549f
kingsley <kingsleyedemudoh@yahoo.com>
2024-09-25 21:15:07 +0100

    added day7 assignment

---
+++ src/main/java/UserDataCheck.java
@@ -0,0 +1,25 @@
+public class UserDataCheck {
+    public static void main(String[] args) {
+        UserDataCheck dataCheck = new UserDataCheck();
+        dataCheck.getUserData(0, "Female");
+    }
+
+    public void getUserData(int age, String gender){
+
+        if((age > 0) && (age < 18) && (gender == "Male")) {
+            System.out.println("User is a boy");
+        }else if((age > 0) && (age < 18) && (gender == "Female")) {
+            System.out.println("User is a girl");
+        }else if((age>=18) && (age <60) && (gender == "Male")){
+            System.out.println("User is a Man");
+        }else if((age>=18) && (age <60) && (gender == "Female")){
+            System.out.println("User is a Woman");
+        }
+        else if(age>=60){
+            System.out.println("User is a Senior Citizen");
+        }
+        else {
+            System.out.println("Please learn more about ages!");
+        }
+    }
+}
