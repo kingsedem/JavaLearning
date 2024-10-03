8e99e387e97b1f9580316a4107d8c36233f86c7d
kingsley <kingsleyedemudoh@yahoo.com>
2024-10-03 09:43:31 +0100

    adding day10

--- src/main/java/FinalDemo.java
+++ src/main/java/FinalDemo.java
@@ -1,14 +1,14 @@
 public class FinalDemo {
 
-    int a;
+    final int a = 20;
 
     public static void main(String[] args) {
         FinalDemo finalDemo = new FinalDemo();
         finalDemo.m1();
-
     }
 
     public void m1(){
+        //a++;
         System.out.println(a);
     }
 }
---
+++ src/main/java/StringDemo.java
@@ -0,0 +1,25 @@
+public class StringDemo {
+
+    public static void main(String[] args) {
+        String name = "ahkiljain";
+
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.toLowerCase());
+//        System.out.println(name.length());
+//        System.out.println(name.charAt(3));
+//        System.out.println(name.substring(3, 6));
+//        System.out.println(name.indexOf("l"));
+//        System.out.println(name.contains("big"));
+        System.out.println(name.replace("a", "b"));
+        String firstName = "Kingsley";
+        String lastName = "Udoh";
+        System.out.println("username: "+ firstName.concat(lastName));
+
+
+//        String  name2 = new String();
+//
+
+//        Animal animal = new Animal();
+//        Animal animal1 = "Dog";
+    }
+}
