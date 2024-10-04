14a7326a96c26d80f828fd7781cfd5951ae954d2
kingsley <kingsleyedemudoh@yahoo.com>
2024-10-04 17:00:48 +0100

    added day11

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,3 +1,19 @@
 public class Animal {
 
+   private String name;
+    public int id;
+
+//    public Animal(){  //This is the default constructor set by JAVA when we don't set anyone ourselves
+//
+//    }
+
+    public Animal(String  animalName, int animalId){  //Constructor with inputs
+     name = animalName;
+     id = animalId;
+    }
+
+    public void getDetails(){
+        System.out.println("Name is "+ name+", id is: "+id);
+    }
+
 }
---
+++ src/main/java/Jungle.java
@@ -0,0 +1,11 @@
+public class Jungle {
+
+    public static void main(String[] args) {
+        Animal lion = new Animal("Lion", 902);
+        lion.getDetails();
+
+        Animal tiger = new Animal("Darix", 2);
+        tiger.getDetails();
+
+    }
+}
